9043d2a6294cc96fc1b8c867a4d5cc75b4d3fce7
hjokinen <oh2ncp@kolumbus.fi>
2018-07-31 13:27:00 +0300

    Waza-ari do not stop osaekomi during golden score. (IJF, what did you think in the first place?)

--- judotimer/stopwatch.c
+++ judotimer/stopwatch.c
@@ -263,17 +263,9 @@ void update_clock(void)
 	    score = 2;
 	if (oElap >= wazaari) {
 	    score = 3;
-	    // waza-ari stops the golden score
-	    if (golden_score && use_2018_rules) {
-		running = FALSE;
-		oRunning = FALSE;
-		gint tmp = osaekomi_winner;
-		give_osaekomi_score();
-		if (tmp)
-		    approve_osaekomi_score(0);
-	    } else if (!use_2017_rules &&
-		       ((osaekomi_winner == BLUE && bluepts[W]) ||
-			(osaekomi_winner == WHITE && whitepts[W]))) {
+	    if (!use_2017_rules &&
+		((osaekomi_winner == BLUE && bluepts[W]) ||
+		 (osaekomi_winner == WHITE && whitepts[W]))) {
 		running = FALSE;
 		oRunning = FALSE;
 		give_osaekomi_score();
