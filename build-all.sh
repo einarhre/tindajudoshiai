9ad60c1b1bf9e9e07d0882c20207aab8e8dbefca
hjokinen <oh2ncp@kolumbus.fi>
2019-04-12 10:29:14 +0300

    Update.

--- build-all.sh
+++ build-all.sh
@@ -3,10 +3,10 @@ make clean
 make clean TARGETOS=WINXP
 make clean TARGETOS=WIN32
 make clean TARGETOS=WIN64
-make && \
-make TARGETOS=WINXP && \
-make TARGETOS=WIN32 && \
-make TARGETOS=WIN64 && \
+make -j && \
+make -j TARGETOS=WINXP && \
+make -j TARGETOS=WIN32 && \
+make -j TARGETOS=WIN64 && \
 make setup TARGETOS=WINXP && \
 make setup TARGETOS=WIN32 && \
 make setup TARGETOS=WIN64
