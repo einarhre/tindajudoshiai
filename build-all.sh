1b8a5386ffc66f50c5a321a9abcaf70c681b8dd9
hjokinen <oh2ncp@kolumbus.fi>
2019-08-14 15:17:00 +0300

    Ver 2.9.3

--- build-all.sh
+++ build-all.sh
@@ -12,3 +12,5 @@ make TARGETOS=WIN64       2>&1 | tee -a $LOG
 make setup TARGETOS=WINXP 2>&1 | tee -a $LOG
 make setup TARGETOS=WIN32 2>&1 | tee -a $LOG
 make setup TARGETOS=WIN64 2>&1 | tee -a $LOG
+cp ~/js-build/release-win*/judoshiai-setup-*.exe ~/d/Public/judoshiai/
+cp ~/js-build/release-linux/judoshiai/judoshiai_*.deb ~/d/Public/judoshiai/
