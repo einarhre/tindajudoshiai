8dc5097ec13aab12850fef3c4cfdd2eaff1fe1be
oh2ncp <oh2ncp@kolumbus.fi>
2023-01-07 13:20:41 +0200

    Update.

--- install-mxe-env.sh
+++ install-mxe-env.sh
@@ -3,4 +3,4 @@
 cd
 git clone https://github.com/mxe/mxe.git
 cd ~/mxe
-make MXE_TARGETS='i686-w64-mingw32.shared x86_64-w64-mingw32.shared' gtk3 curl librsvg libao mpg123
+make MXE_TARGETS='i686-w64-mingw32.shared x86_64-w64-mingw32.shared' gtk3 curl librsvg libao mpg123 pthreads libuv gnutls
